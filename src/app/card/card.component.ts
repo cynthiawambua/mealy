cec696830971569e3c9f9e1775e8f1f96e3cdd96
Byron Jenkins <codeherk@gmail.com>
2018-11-26 16:51:38 -0500

    Implemented card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card', // the selector is what will be used in the tag ex. <app-card> </app-card>
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
