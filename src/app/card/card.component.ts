41fc467ff6145555dd28cc9c5781479b22f157f3
Cynthia <cynthiawambua@gmail.com>
2018-12-10 00:28:27 -0500

    accesss firebase sub collections

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -15,7 +15,8 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 export class CardComponent implements OnInit {
   modalRef: BsModalRef;
   trucks: Observable<any[]>;
-  menu: Observable<any[]>;
+  entrees: Observable<{}>;
+  sides: Observable<{}>;
 
   constructor(private modalService: BsModalService, db: AngularFirestore){
     this.trucks = db.collection('trucks').valueChanges();
@@ -23,18 +24,38 @@ export class CardComponent implements OnInit {
     // view debugger console on browser and look at what's being logged below
     console.log(this.trucks); // observable
     this.trucks.subscribe(data => console.log(data)); // must subscribe observable to get data
- 
-    /*method to access sub-collections
-    this.menu = db.collection('Menu').valueChanges();
-    console.log(this.menu); // observable
-    this.menu.subscribe(data => console.log(data)); // must subscribe observable to get data
-    */
- 
- 
+  
+    this.entrees = db.collection('trucks').doc('Burger Tank').collection('Entrees').valueChanges();
+    console.log(this.entrees);
+    this.entrees.subscribe(data => console.log(data));
+  
+    this.sides = db.collection('trucks').doc('Burger Tank').collection('Sides').valueChanges();
+    console.log(this.sides);
+    this.sides.subscribe(data => console.log(data));
+
+  
   }
 
- 
 
+    getDocument() {
+      var db: AngularFirestore;
+    // [START get_document]
+    var truckRef = db.collection('trucks').doc('Burger Tank');
+    var getDoc = truckRef.get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          console.log('Document data:', doc.data());
+        }
+      })
+      .catch(err => {
+        console.log('Error getting document', err);
+      });
+    // [END get_document]
+  
+   return getDoc;
+  }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
